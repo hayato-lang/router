a75f0cdfb531a53a1b027d1479f3c89587d2f6cc
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-21 18:21:02 +0900

    ネストされたページへ遷移

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css';
 import { Home } from "./Home";
 import { Page1 } from './Page1';
 import { Page2 } from './Page2';
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 
 function App() {
   return (
@@ -20,9 +22,19 @@ function App() {
       <Route exact path="/">
         <Home/>
         </Route>
-        <Route path="/page1">
-        <Page1 />
-        </Route>
+        <Route path="/page1" render={({ match: { url } }) => (
+          <Switch>
+            <Route exact path={url}>
+              <Page1 />
+            </Route>
+            <Route exact path={`${url}/detailA`}>
+              <Page1DetailA />
+            </Route>
+            <Route exact path={`${url}/detailB`}>
+              <Page1DetailB />
+            </Route>
+          </Switch>
+        )} />
         <Route path="/page2">
         <Page2 />
         </Route>
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1です。</h1>
+      <Link to="/Page1/detailA">Page1DetailA</Link>
+      <br />
+      <Link to="/Page1/detailB">Page1DetailB</Link>
     </div>
   );
 };
