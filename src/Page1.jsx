81d17169de3a4e82b53ccede9b97e044b90ee66e
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-22 09:07:08 +0900

    stateを渡すページ遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
   return (
     <div>
       <h1>Page1です。</h1>
-      <Link to="/Page1/detailA">Page1DetailA</Link>
+      <Link to={{ pathname: "/Page1/detailA", state: arr }}>Page1DetailA</Link>
       <br />
       <Link to="/Page1/detailB">Page1DetailB</Link>
     </div>
