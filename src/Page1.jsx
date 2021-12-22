24e5c50dce07c35cb4ab59607dbd64223ddfad5b
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-22 13:12:41 +0900

    Linkを使わない画面遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,13 +1,18 @@
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
+  const history = useHistory();
+  const onClickDetailA = () => history.push("/page1/detailA");
   return (
     <div>
       <h1>Page1です。</h1>
       <Link to={{ pathname: "/Page1/detailA", state: arr }}>Page1DetailA</Link>
       <br />
       <Link to="/Page1/detailB">Page1DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
